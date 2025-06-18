b26f443d1fd90fce3a619d213d5aea11a6acbd33
emily405 <197441060+emily405@users.noreply.github.com>
2025-06-18 17:29:58 +0100

    Create generate_webpage.py

---
+++ generate_webpage.py
@@ -0,0 +1,68 @@
+from git import Repo
+import json
+import time
+
+repo = Repo('.')
+assert not repo.bare
+
+all_commits = list(repo.iter_commits("main", paths='yeehaw.json'))
+
+html = '''<!doctype html>
+<html>
+
+<head>
+    <title>test playlist tracker</title>
+    <meta name="description" content="Tracking songs added and removed form the yeehaw playlist">
+    <meta name="keywords" content="yeehaw playlist spotify">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="style.css" rel="stylesheet" type="text/css" media="all">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis">
+</head>
+
+<body>
+
+    <h1>test playlist tracker</h1>
+
+    <a id="test_section"></a>
+    <h2>Test Section</h2>
+
+'''
+
+
+for commit in all_commits[:-1]:
+    f0 = json.load(commit.tree['test_playlist.json'].data_stream)
+    f0_date = commit.committed_datetime
+    commit_prev = commit.parents[0]
+    f1 = json.load(commit_prev.tree['test_playlist.json'].data_stream)
+    added_songs = [s for s in f0 if s not in f1]
+    removed_songs = [s for s in f1 if s not in f0]
+
+    for addition in added_songs:
+        newrow = f'''<br>
+        <span class="date">{addition["added"]}</span>
+        <span class="pm">+</span>
+        <span class="song">{addition["name"]}</span>
+        <iframe src="https://open.spotify.com/embed/track/{addition["id"]}" ></iframe> '''
+
+        html = html + newrow
+
+    for removal in removed_songs:
+        newrow = f'''<br>
+        <span class="date">{f0_date}</span>
+        <span class="pm">+</span>
+        <span class="song">{removal["name"]}</span>
+        <iframe src="https://open.spotify.com/embed/track/{removal["id"]}" ></iframe> '''
+
+        html = html + newrow
+
+
+
+endstring = '''</body>
+
+</html>
+'''
+
+html = html + endstring
+
+with open("index.html", "w") as f:
+    f.write(html)
